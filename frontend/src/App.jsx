15566e3ad267cfe02689c75819ab5fa6b7c38d7e
karan <morekaran236@gmail.com>
2023-04-13 23:40:54 +0530

    front end components added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
-
+import Navbar from './components/Navbar'
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/Home'
+import Register from './components/Register'
+import Signin from './components/Signin'
 function App() {
 
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+     </Routes>
+    </>
   )
 }
 
